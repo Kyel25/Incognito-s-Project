0ac19c5ae1b11cf032eaaadabf88ea074e213f04
Kyel25 <125393560+Kyel25@users.noreply.github.com>
2023-04-12 14:01:14 +0000

    Adidas Page/Twitter Timeline Feed 10:00am 12/4/2023

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-$(".hover").mouseleave(
-  function() {
-    $(this).removeClass("hover");
-  }
-);
+function loadSearchData() {
+  // Data to be used in the searchbar
+  let countries = [
+    'Adidas',
+    'Puma',
+    'Nike',
+    'The North Face',
+  ];
 
+  // Get the HTML element of the list
+let list = document.getElementById('list');
+// Add each data item as an <a> tag
+countries.forEach((country)=>{
+    let a = document.createElement("a");
+    a.innerText = country;
+    a.classList.add("listItem");
+    list.appendChild(a);
+})
+}
+
+function search(){
+  let listContainer = document.getElementbyID('list');
+  let listItems = document.getElementsbyClassName('listItem');
+  let input = document.getElementbyID('searchbar');
+  input = input.toLowerCase();
+
+  let noResults = true;
+for (i = 0; i < listItems.length; i++) { 
+    if (!listItems[i].innerHTML.toLowerCase().includes(input) || input === "") {
+        listItems[i].style.display="none";
+        continue;
+    }
+    else {
+        listItems[i].style.display="flex";
+        noResults = false; 
+    }
+  else {
+    listContainer.style.display = noResults ? "none" : "block";
+  }
+}
+  
+}
